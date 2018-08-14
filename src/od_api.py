d0d6116df6e8f676e63617cec0fc1122170c7ce4
Carine Dengler <carine-dengler@zo.uni-heidelberg.de>
2018-08-14 11:01:06 +0200

    added od_api.py

---
+++ src/od_api.py
@@ -0,0 +1,43 @@
+#    OpenDACHS 1.0
+#    Copyright (C) 2018  Carine Dengler
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+:synopsis: API call.
+"""
+
+
+# standard library imports
+import subprocess
+
+# third party imports
+# library specific imports
+
+
+def api_call(args):
+    """API call.
+
+    :param list args: command-line arguments
+    """
+    try:
+        args = [
+            "docker", "exec", "-it", "webrecorder_app_1",
+            "python3", "-m",  "webrecorder.od_webrecorder", *args
+        ]
+        subprocess.Popen(args, stderr=subprocess.PIPE)
+    except Exception as exception:
+        raise RuntimeError("failed to call API\t: {}".format(exception))
+    return
