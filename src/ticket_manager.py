852fd8e6903fe7e154418f107d88cfcbdf50cc0d
Carine Dengler <carine-dengler@zo.uni-heidelberg.de>
2018-10-18 10:23:10 +0200

    ticket_manager.py
    - fixed parameters

--- src/ticket_manager.py
+++ src/ticket_manager.py
@@ -468,7 +468,7 @@ class TicketManager(object):
             logger = logging.getLogger().getChild(self.remove_expired.__name__)
             sqlite_client = src.sqlite.SQLiteClient(self.sqlite)
             rows = sqlite_client.select_rows(
-                "timestamp", ("date('now', '-3 day')")
+                "timestamp", ("date('now', '-3 day')",)
             )
             tickets = [src.ticket.Ticket.get_ticket(row) for row in rows]
             counter = 0
